31425a9d59b90a3652bf73f9063f4d5c3524690f
renato <renatocarrino@gmail.com>
2023-12-21 11:39:01 -0300

    Connection to MongoDB created

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,11 +7,13 @@ import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true} path="/" element={<HomeScreen/>}></Route>  {/* index is to ensure that the HomeScreen only will be shown */}
+      <Route path="/product/:id" element={<ProductScreen/>}></Route>  
     </Route>
   )
 )
