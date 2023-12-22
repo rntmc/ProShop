05d49da413436c4def77e764131f83ceed3ed238
renato <renatocarrino@gmail.com>
2023-12-22 11:29:12 -0300

    creation of authSlice and usersApiSlice and LoginScreen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,13 +11,15 @@ import reportWebVitals from './reportWebVitals';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App/>}>
       <Route index={true} path="/" element={<HomeScreen/>}></Route>  {/* index is to ensure that the HomeScreen only will be shown */}
       <Route path="/product/:id" element={<ProductScreen/>}></Route>  
-      <Route path="/cart" element={<CartScreen/>}></Route>  
+      <Route path="/cart" element={<CartScreen/>}></Route>
+      <Route path="/login" element={<LoginScreen/>}></Route>
     </Route>
   )
 )
