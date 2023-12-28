1d1afeadf8470ed5fa80ef732987dfe205152c82
renato <renatocarrino@gmail.com>
2023-12-28 16:28:20 -0300

    review implemented

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -43,6 +43,14 @@ export const productsApiSlice = apiSlice.injectEndpoints({
         url: `${PRODUCTS_URL}/${productId}`,
         method: 'DELETE'
       })
+    }),
+    createReview: builder.mutation({
+      query: (data) => ({
+        url: `${PRODUCTS_URL}/${data.productId}/reviews`,
+        method: 'POST',
+        body: data,
+      }),
+      invalidatesTags: ['Product'],
     })
   }),
 });
@@ -54,4 +62,5 @@ export const {
   useUpdateProductMutation, 
   useUploadProductImageMutation,
   useDeleteProductMutation,
+  useCreateReviewMutation,
 } = productsApiSlice; //convention whne exporting const
